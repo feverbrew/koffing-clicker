3d5544c4cc3efa2f0fd3bf8d1e3ca4f69cee94c5
feverbrew <33206171+feverbrew@users.noreply.github.com>
2021-06-22 14:10:33 -0400

    Local Storage Implemented
    Implemented local storage for gas count. Gas count will now be maintained even upon closing the browser instance.

--- src/koffing.js
+++ src/koffing.js
@@ -1,9 +1,12 @@
-let count = 0;
+let gasCount = localStorage.getItem("gasCount") || 0;
 
 let clicks = document.getElementById('click-counter');
 let koffingClick = document.getElementById('the-koffing'); // Some jerk on the internet told me to put a . before the-koffing. They lied.
 
+clicks.innerText = "Gas: " + gasCount;
+
 function incrementCounter() {
-    count++
-    clicks.innerText = "Gas: " + count;
+    gasCount++
+    clicks.innerText = "Gas: " + gasCount;
+    localStorage.setItem("gasCount", gasCount);
 }
